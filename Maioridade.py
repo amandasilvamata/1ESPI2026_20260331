b9ff5012f3665531e4ab9c50f0a0d8613b40e1f7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-31 09:15:29 -0300

    Initial commit

---
+++ Maioridade.py
@@ -0,0 +1,15 @@
+# titulo = 'Maioridade'
+# print(f'{titulo: ^30}')
+# idade = int(input('Que idade você tem? '))
+# if idade >= 18: #sim, then, 1
+#     print('Você é maior de idade!')
+# print('Obrigada por participar da pesquisa!')
+
+titulo = 'Maioridade'
+print(f'{titulo: ^30}')
+idade = int(input('Que idade você tem? '))
+if idade >= 18: #sim, then, 1
+    print('Você é maior de idade!')
+else: #não, else, 0
+    print('Você é menor de idade')
+print('Obrigada por participar da pesquisa!')
