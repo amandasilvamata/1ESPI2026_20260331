c9b12667f28bcfdeba9222def8a510f1149792fc
amandasilvamata <166837389+amandasilvamata@users.noreply.github.com>
2026-03-31 11:27:31 -0300

    Update exerc2.py

--- exerc2.py
+++ exerc2.py
@@ -1,2 +1,10 @@
+titulo = 'Sindico'
+print(f'{titulo:^30}')
 
+# numero par
+bloco = int(input('Entre com o bloco 1 a 20: '))
 
+if 1<= bloco <= 10:
+    print('Sindico Hamilton')
+else:
+    print('Sindico Jose')
